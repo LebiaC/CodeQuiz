ebc44ae5e1b8730c5daf8154ace01d5e13fe8094
Lebia Chatman <lebiachatman@gmail.com>
2020-12-07 17:53:26 -0600

    Added Event Listener

--- scrpit.js
+++ scrpit.js
@@ -37,9 +37,24 @@ var questions= [
 var otherMoreVerboseFullForNosReasonNick = document.getElementById('nick')//element selection (superflous b/c html id attribute creates a reference anyway)
 console.log(nick)
 console.log(nick==otherMoreVerboseFullForNosReasonNick)
-nick.addEventListener('click', function(){
+var isItAThing = nick.addEventListener('click', function(){
     var questionOb = questions[0]
+    doFirstQ(questionOb)
+})
+console.log(isItAThing)
+
+function doFirstQ(quest){
+    //nick.removeEventListn
+    var questionOb = quest
     var firstq=questionOb.question
     nick.innerHTML = "<h3>"+firstq+"</h3>"
-    nick.innerHTML += "<h6>"+ questionOb.choices+"</h6>"
-})
+    var firstChoice =document.createElement('p')
+    firstChoice.textContent=questionOb.choices[0]
+    console.log(firstChoice)
+    console.log(typeof firstChoice)
+    firstChoice.addEventListener('click', function(){
+        console.log("nick")
+    })
+    nick.appendChild(firstChoice)
+    //nick.innerHTML += "<h6>"+ questionOb.choices[1]+"</h6>"
+}
